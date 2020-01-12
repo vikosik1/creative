be537fdf3b2a9a60b980a3597213b29f61e0e93e
vikosik1 <vikosik1@ukr.net>
2020-01-12 17:18:57 +0200

    search

--- js/app.js
+++ js/app.js
@@ -1,15 +1,26 @@
 $(document).ready(function() {
-    $('.ba-menu-btn').on('click', function(){
+    $('.ba-menu-btn').on('click', function() {
         $('.ba-menu').toggle();
     });
 
     $(window).on('resize', function() {
-        
-        if ($(this).width() > 991 ) {
+
+        if ($(this).width() > 991) {
             $('.ba-menu').show();
         } else {
             $('.ba-menu').hide();
         }
 
     })
+
+    $('#showSearch').on('click', function() {
+        $('.ba-search-form').toggleClass('open')
+        $('#showSearch i').toggleClass('fa-times')
+
+    })
+
+
+
+
+
 })
